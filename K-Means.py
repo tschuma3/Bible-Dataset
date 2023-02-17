1424fef9114f4f403d13195d343036ab1626b4b9
Tyler Schumacher <tschuma3@uccs.edu>
2023-02-16 18:55:17 -0700

    Finished implementing KMeans and saved data to a csv file

--- K-Means.py
+++ K-Means.py
@@ -1,28 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import mplcursors
 import pandas as pd
-import re
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.cluster import KMeans
 
 #Importing the dataset
 dataset = pd.read_csv(r'BibleData-Commandments.csv')
 X = dataset.iloc[:, [1, 3]].values
+actual_values_X = dataset.iloc[:, [1, 3]].values #This is for the actual names
 print(X)
 
-
+#Encoding the two columns
 le = LabelEncoder()
 X[:, 0] = le.fit_transform(X[:, 0])
 X[:, 1] = le.fit_transform(X[:, 1])
 
-"""
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-"""
 print(X)
 print(X.shape)
-
 
 #Using the elbow method to find the optimal number of clusters
 wcss = []
@@ -33,7 +28,7 @@ for i in range(1, 11):
 plt.plot(range(1, 11), wcss)
 plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
+plt.ylabel('WCSS') #WCSS = Sum squared distance between each point and the centroid in a cluster
 plt.show()
 
 
@@ -42,6 +37,7 @@ kmeans = KMeans(n_clusters=6, init='k-means++', random_state=42)
 y_kmeans = kmeans.fit_predict(X)
 
 print(y_kmeans)
+print(y_kmeans.dtype)
 
 #Visualizing the clusters
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
@@ -52,7 +48,20 @@ plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', la
 plt.scatter(X[y_kmeans == 5, 0], X[y_kmeans == 5, 1], s = 100, c = 'orange', label = 'Cluster 6')
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
 plt.title('Bible Commandments')
-plt.xlabel('Mishneh Torah Book Name')
-plt.ylabel('Mishneh Torah Category')
+plt.xlabel('Commandments')
+plt.ylabel('Scripture Reference')
 plt.legend()
+#mplcursors.cursor(hover=True) #Uncommment this to hover labels for exact points
 plt.show()
+
+#Converting y_kmeans, X, and actual_values_X to pandas dataframes
+y_kmeans = pd.DataFrame(y_kmeans, columns=['Which Cluster'])
+X = pd.DataFrame(X, columns=['Numerical Commandment Reference', 'Numerical Bible Verse Reference'])
+actual_values_X = pd.DataFrame(actual_values_X, columns=['Commandment', 'Bible Verse'])
+print(y_kmeans)
+print(X)
+print(actual_values_X)
+
+#Join all the dataframes and save to a csv file
+X = pd.concat([y_kmeans, actual_values_X, X], axis=1)
+X.to_csv('Bible_Commandments_Clustering.csv', index=False)
